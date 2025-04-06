a09ddb405368a14837817f7d6fccac79b5ef56b2
FrankNPC <cmings.chen@gmail.com>
2025-04-06 01:53:48 -0400

    improve

--- src/test/java/io/steplogs/spring/rmi/http/subscriber/ExampleServiceSubscriber.java
+++ src/test/java/io/steplogs/spring/rmi/http/subscriber/ExampleServiceSubscriber.java
@@ -1,15 +1,18 @@
 package io.steplogs.spring.rmi.http.subscriber;
 
 
+import java.util.concurrent.TimeUnit;
+
+import org.apache.hc.client5.http.config.ConnectionConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
-
-import jakarta.annotation.Resource;
 
 @Configuration // declare @Configuration to scan beans
 public class ExampleServiceSubscriber<T> extends AbstractServiceSubscriber implements ServiceClientTemplate<T> {
@@ -22,15 +25,32 @@ public class ExampleServiceSubscriber<T> extends AbstractServiceSubscriber imple
 		return host;// must have url
 	}
 
-	@Resource 
-	CloseableHttpClient closeableHttpClient;
+	// I use a http connection pool for RestClient
+	private CloseableHttpClient httpClient() {
+		PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+		connectionManager.setMaxTotal(1024);
+		connectionManager.setDefaultMaxPerRoute(1024);
+		connectionManager.setConnectionConfigResolver(route -> {
+			return ConnectionConfig.custom()
+					.setConnectTimeout(15, TimeUnit.SECONDS)
+					.setSocketTimeout(15, TimeUnit.SECONDS)
+					.setTimeToLive(2, TimeUnit.MINUTES)
+					.setValidateAfterInactivity(30, TimeUnit.SECONDS)
+					.build();
+		});
 
+		return HttpClients.custom()
+				.setConnectionManager(connectionManager)
+				.setConnectionManagerShared(true)
+				.build();
+	}
+	
 	@Override
 	public RestClient getRestClient() {
 		return RestClient
-			.builder(new RestTemplate(new HttpComponentsClientHttpRequestFactory(closeableHttpClient)))
+			.builder(new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient())))
 			.baseUrl(getBaseUrl())
-//            .messageConverters(HttpMessageConverter)
+//            .messageConverters(HttpMessageConverter) // customize pay load convertors instead of default
 			.build();
 	}
 
